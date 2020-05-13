8840a47a66434070562995ef84eb1da05fe26374
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2020-05-13 22:30:04 +0200

    Updates

---
+++ precache-manifest.36e2678766c9c9efe7f0809b3ed7dde3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb0debbf4f59b3f2ef3c83ef58321a5",
+    "url": "/CheckBox/index.html"
+  },
+  {
+    "revision": "cead3e8a60aad3fc6f1f",
+    "url": "/CheckBox/static/css/main.d2069e48.chunk.css"
+  },
+  {
+    "revision": "1ed3be6eb5c93075e238",
+    "url": "/CheckBox/static/js/2.af8b5af6.chunk.js"
+  },
+  {
+    "revision": "cead3e8a60aad3fc6f1f",
+    "url": "/CheckBox/static/js/main.d2d80d5d.chunk.js"
+  },
+  {
+    "revision": "609b630eb32e06dccc29",
+    "url": "/CheckBox/static/js/runtime~main.d7e9b288.js"
+  }
+]);
